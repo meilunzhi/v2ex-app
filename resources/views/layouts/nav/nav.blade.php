6cf49bae4869aba682f727629614ea57cf488191
Magicians <meilunzhi@gmail.com>
2015-09-04 23:51:54 +0800

    update to 5.1.16

---
+++ resources/views/layouts/nav/nav.blade.php
@@ -0,0 +1,18 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <!-- Latest compiled and minified CSS -->
+    <link rel="stylesheet" href="{{ assert }}">
+
+    <!-- Optional theme -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap-theme.min.css">
+
+    <!-- Latest compiled and minified JavaScript -->
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
+    <title>Document</title>
+</head>
+<body>
+
+</body>
+</html>
--- vendor/laravel/framework/src/Illuminate/Foundation/Auth/Access/AuthorizesRequests.php
+++ vendor/laravel/framework/src/Illuminate/Foundation/Auth/Access/AuthorizesRequests.php
@@ -10,7 +10,7 @@ trait AuthorizesRequests
     /**
      * Authorize a given action against a set of arguments.
      *
-     * @param  string  $ability
+     * @param  mixed  $ability
      * @param  mixed|array  $arguments
      * @return void
      *
@@ -18,11 +18,7 @@ trait AuthorizesRequests
      */
     public function authorize($ability, $arguments = [])
     {
-        if (func_num_args() === 1) {
-            $arguments = $ability;
-
-            $ability = debug_backtrace(false, 2)[1]['function'];
-        }
+        list($ability, $arguments) = $this->parseAbilityAndArguments($ability, $arguments);
 
         if (! app(Gate::class)->check($ability, $arguments)) {
             throw $this->createGateUnauthorizedException($ability, $arguments);
@@ -33,19 +29,37 @@ trait AuthorizesRequests
      * Authorize a given action for a user.
      *
      * @param  \Illuminate\Contracts\Auth\Authenticatable|mixed  $user
-     * @param  string  $ability
-     * @param  array  $arguments
+     * @param  mixed  $ability
+     * @param  mixed|array  $arguments
      * @return void
      *
      * @throws \Symfony\Component\HttpKernel\Exception\HttpException
      */
-    public function authorizeForUser($user, $abiility, $arguments = [])
+    public function authorizeForUser($user, $ability, $arguments = [])
     {
+        list($ability, $arguments) = $this->parseAbilityAndArguments($ability, $arguments);
+
         $result = app(Gate::class)->forUser($user)->check($ability, $arguments);
 
         if (! $result) {
             throw $this->createGateUnauthorizedException($ability, $arguments);
         }
+    }
+
+    /**
+     * Guesses the ability's name if it wasn't provided.
+     *
+     * @param  mixed  $ability
+     * @param  mixed|array  $arguments
+     * @return array
+     */
+    protected function parseAbilityAndArguments($ability, $arguments)
+    {
+        if (is_string($ability)) {
+            return [$ability, $arguments];
+        }
+
+        return [debug_backtrace(false, 3)[2]['function'], $ability];
     }
 
     /**
